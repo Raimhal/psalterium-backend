1b1e33c37e325045351d98a8ed9d5c120853a579
Raimhal <kormish2003@gmail.com>
2022-05-24 17:20:15 +0300

    cors changes

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from starlette.types import ASGIApp, Scope, Receive, Send
 app = FastAPI()
 
 origins = [
-    "*"
+    "http://localhost",
+    "https://psalterium-backend.herokuapp.com"
 ]
 
 app.add_middleware(
